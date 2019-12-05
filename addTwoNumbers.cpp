a6d94977944af89885053d0061605228233ac69b
CheesyPizza <gajansuryan@gmail.com>
2019-12-05 18:47:34 -0500

    Create addTwoNumbers.cpp

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,73 @@
+
+
+/**
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ 
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //create a new node if the first 
+        ListNode* result=new ListNode(0);
+        ListNode* resultHead =result;
+        ListNode* prev;
+        ushort remainder=0;
+        while(l1 !=NULL || l2 !=NULL)
+        {
+            int sum=(l1?l1->val:0)+(l2?l2->val:0)+remainder;
+            remainder=sum/10;
+            result->val=sum %10;     
+            prev=result;
+            result->next=new ListNode(0);
+            result = result->next;
+            if(l2!=NULL)
+                l2=l2->next;
+            if(l1!=NULL)
+                l1=l1->next;
+        }
+        if (remainder==1)
+        {
+           result->val=1; 
+        }
+        else{
+             prev->next=NULL;
+        delete result;
+        }
+        return resultHead;
+    }     
+};
+
+/*
+class Solution {
+public:
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode preHead(0), *p = &preHead;
+    int extra = 0;
+    while (l1 || l2 || extra) {
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+        extra = sum / 10;
+        p->next = new ListNode(sum % 10);
+        p = p->next;
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
+    }
+    return preHead.next;
+}
+};
+*/
